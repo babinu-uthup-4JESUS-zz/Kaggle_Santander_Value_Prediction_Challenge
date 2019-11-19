b37629bf796d4e14260abdc33e23e8539807ef28
babinu-uthup-4JESUS <babinu@gmail.com>
2019-11-19 13:06:07 -0500

    Variants of gradient boosting models using xgboost.

---
+++ common_routines/relevant_functions.py
@@ -0,0 +1,72 @@
+import numpy as np
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Imputer
+from sklearn import linear_model
+
+def evaluate_model_score(my_model, X, Y):
+    predictions = my_model.predict(X)
+    return evaluate_model_score_given_predictions(predictions, Y)
+
+def make_predictions(my_model, X):
+    predictions = my_model.predict(X)
+    return predictions
+
+def evaluate_model_score_given_predictions2(predictions, Y):
+    mean_of_squared_error1 = \
+        mean_squared_error((np.log(np.abs(Y + 1))), (np.log(np.abs(predictions + 1))))
+    return np.sqrt(mean_of_squared_error1)
+
+def evaluate_model_score_given_predictions(predictions, Y):
+    mean_of_squared_error1 = \
+        mean_squared_error(Y, predictions)
+    return np.sqrt(mean_of_squared_error1)
+
+def evaluate_neg_model_score(my_model, X, Y):
+    return (-1) * evaluate_model_score(my_model, X, Y)
+
+
+def cross_val_scores_given_model(my_model, X, Y, cv=5):
+    cross_val_score1 = cross_val_score(my_model, 
+                                       X, Y, 
+                                       scoring=evaluate_model_score,
+                                       cv=cv)
+    return cross_val_score1
+
+def cross_val_score_given_model(my_model, X, Y, cv=5):
+    cross_val_score1 = cross_val_score(my_model, 
+                                       X, Y,
+                                       scoring=evaluate_model_score, 
+                                       cv=cv)
+                                       
+    return cross_val_score1.mean()
+
+def fit_pipeline_and_cross_validate(my_pipeline,
+                                    train_data,
+                                    X_columns,
+                                    Y_column='new_target'):
+    X = train_data[X_columns]
+    Y = train_data[[Y_column]].values.ravel()
+    my_pipeline.fit(X, Y)
+    return (my_pipeline, cross_val_score_given_model(my_pipeline, X, Y))
+
+def get_rel_cols(percent_threshold, data):
+    cols_apart_from_id_and_target = [col for col in data.columns if col not in ['ID', 'target', 'new_target']]
+
+    non_zero_percent_for_col = np.zeros(len(cols_apart_from_id_and_target))
+
+    for i in range(len(cols_apart_from_id_and_target)):
+        non_zero_percent_for_col[i] = \
+            100.0* len(data[cols_apart_from_id_and_target[i]].to_numpy().nonzero()[0])/\
+        len(data[cols_apart_from_id_and_target[i]].to_numpy())    
+
+    num_rel_entries = np.sum(non_zero_percent_for_col > percent_threshold)
+
+    rel_col_indices = np.argsort(non_zero_percent_for_col)[::-1][0:num_rel_entries]
+
+    rel_cols = [cols_apart_from_id_and_target[col] for col in rel_col_indices]
+    
+    return rel_cols
